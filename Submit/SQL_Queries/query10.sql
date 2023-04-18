fb2532757f68336e8e8ca44d03f7b36bb78cdac3
Lohitha-Reddy <nimmalohitha@gmail.com>
2023-04-17 22:31:09 -0400

    clean up

--- Submit/SQL_Queries/query10.sql
+++
@@ -1,7 +0,0 @@
-SELECT product_feedback.Review_Body, product_feedback.Rating, product_feedback.Review_Date,product.Name, product.Brand,product.Price
-FROM product_feedback
-JOIN product
-ON product_feedback.Product_ID = product.Product_ID
-WHERE product_feedback.Rating=1 OR product_feedback.Rating=5
-GROUP BY product_feedback.Product_ID
-ORDER BY MIN(product_feedback.Rating) DESC;
